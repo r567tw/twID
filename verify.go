82492d5cf66738d86e904aabd20b9f67b9ff0d25
r567tw <r567tw@gmail.com>
2023-04-22 12:59:22 +0800

    move local_table into function

--- verify.go
+++ verify.go
@@ -1,4 +1,5 @@
-package twID // Package twID provides a function to verify Taiwanese ID numbers.
+// Package twID provides a function to verify Taiwanese ID numbers.
+package twID
 
 import (
 	"fmt"
@@ -7,16 +8,14 @@ import (
 	"strings"
 )
 
-var local_table = map[string]string{
-	"A": "10", "B": "11", "C": "12", "D": "13", "E": "14", "F": "15", "G": "16", "H": "17", "I": "34",
-	"J": "18", "K": "19", "L": "20", "M": "21", "N": "22", "O": "35", "P": "23", "Q": "24", "R": "25",
-	"S": "26", "T": "27", "U": "28", "V": "29", "W": "32", "X": "30", "Y": "31", "Z": "33",
-}
-
-// Package twID provides a function to verify Taiwanese ID numbers.
 // Verify checks if a Taiwanese ID number is valid or not.
 // Returns true if the ID number is valid, otherwise false.
 func Verify(IDNumber string) bool {
+	var local_table = map[string]string{
+		"A": "10", "B": "11", "C": "12", "D": "13", "E": "14", "F": "15", "G": "16", "H": "17", "I": "34",
+		"J": "18", "K": "19", "L": "20", "M": "21", "N": "22", "O": "35", "P": "23", "Q": "24", "R": "25",
+		"S": "26", "T": "27", "U": "28", "V": "29", "W": "32", "X": "30", "Y": "31", "Z": "33",
+	}
 	id := strings.ToUpper(IDNumber)
 
 	match, err := regexp.MatchString("^[A-Z][1289][0-9]{8}$", id)
