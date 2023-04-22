253b7774e01c88997ed523f125e1262bb9ac3e98
r567tw <r567tw@gmail.com>
2023-04-22 13:01:21 +0800

    format

--- verify.go
+++ verify.go
@@ -11,11 +11,6 @@ import (
 // Verify checks if a Taiwanese ID number is valid or not.
 // Returns true if the ID number is valid, otherwise false.
 func Verify(IDNumber string) bool {
-	var local_table = map[string]string{
-		"A": "10", "B": "11", "C": "12", "D": "13", "E": "14", "F": "15", "G": "16", "H": "17", "I": "34",
-		"J": "18", "K": "19", "L": "20", "M": "21", "N": "22", "O": "35", "P": "23", "Q": "24", "R": "25",
-		"S": "26", "T": "27", "U": "28", "V": "29", "W": "32", "X": "30", "Y": "31", "Z": "33",
-	}
 	id := strings.ToUpper(IDNumber)
 
 	match, err := regexp.MatchString("^[A-Z][1289][0-9]{8}$", id)
@@ -27,6 +22,12 @@ func Verify(IDNumber string) bool {
 
 	if !match {
 		return false
+	}
+
+	var local_table = map[string]string{
+		"A": "10", "B": "11", "C": "12", "D": "13", "E": "14", "F": "15", "G": "16", "H": "17", "I": "34",
+		"J": "18", "K": "19", "L": "20", "M": "21", "N": "22", "O": "35", "P": "23", "Q": "24", "R": "25",
+		"S": "26", "T": "27", "U": "28", "V": "29", "W": "32", "X": "30", "Y": "31", "Z": "33",
 	}
 
 	// Convert the first letter of the ID number to its corresponding number.
